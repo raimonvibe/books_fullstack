54a55159502a9b56d75992a5a2f191a75bba9918
raimonvibe <robertjanstefan@gmail.com>
2024-09-19 21:31:49 +0200

    Add files via upload

---
+++ instance/config.py
@@ -0,0 +1,3 @@
+# instance/config.py
+SECRET_KEY = 'a_very_secret_key'
+SQLALCHEMY_DATABASE_URI = 'mysql://user:password@localhost/db_name'
---
+++ run.py
@@ -0,0 +1,6 @@
+from app import app
+
+if __name__ == "__main__":
+    print("Running on http://127.0.0.1:5000/")
+    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
